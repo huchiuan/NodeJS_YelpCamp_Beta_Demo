97c8faa33688e448b54c4ec279493e63b11ff56a
huchiuan <tusna08124@gmail.com>
2021-02-18 12:42:27 +0800

    <製作edit campus頁面跟其rout>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const path = require ('path');
 const mongoose=require('mongoose');
+const methodOverride = require('method-override');
 const Campground=require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -24,6 +25,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views')); //去views資料夾拿ejs
 
 app.use(express.urlencoded({extended:true})); //可以解析req內的東西
+app.use(methodOverride('_method'));
 
 app.get('/',(req,res)=>{
     res.send('home')
@@ -42,7 +44,7 @@ app.get('/campgrounds/new',(req,res)=>{
  })
 
 app.post('/campgrounds',async(req,res)=>{
-    console.log(req.body.campground);
+    console.log(req.body.campground);//{ title: '333333', location: '33322' }
     const campground = await Campground(req.body.campground);
     await campground.save();//moogose的語法
     res.redirect(`/campgrounds/${campground._id}`)
@@ -55,6 +57,16 @@ app.get('/campgrounds/:id',async(req,res)=>{
  })
 
 
+ app.get('/campgrounds/:id/edit',async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit',{campground});
+ })
+
+ app.put('/campgrounds/:id',async(req,res)=>{
+    const{id} =req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`)
+ })
 
 app.listen(3000,()=>{
    console.log('Serving on port 3000')
